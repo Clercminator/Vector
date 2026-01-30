205c3924e7c2afbf86cb7576fdf1f8f8869e1546
David <clerc.de@outlook.com>
2026-01-30 17:59:03 -0400

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import './styles/index.css'
 import App from './app/App.tsx'
 import { ThemeProvider } from 'next-themes'
 import { LanguageProvider } from './app/components/language-provider.tsx'
 import { ErrorBoundary } from './app/components/ErrorBoundary.tsx'
+
+injectSpeedInsights()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
